620d656b4dc1b0e633f29a7023219a7820c749de
vireshpatel43 <viresh.p2003@gmail.com>
2024-09-22 18:58:53 -0400

    Create DTO for different user types

---
+++ peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/dto/ProfessorDTO.java
@@ -0,0 +1,10 @@
+package com.monaco.peer_assessment_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfessorDTO extends UserDTO {
+
+}
