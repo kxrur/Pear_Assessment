ee3cd79f62559f70ea19a5c2ceb6733b07f7794e
kxr <kxrury@gmail.com>
2024-09-23 16:32:35 -0400

    teacher registration form very similar to user

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import RegisterStudent from './components/views/registration/student'
+import RegisterTeacher from './components/views/registration/teacher'
 
 function App() {
 
   return (
     <div className='bg-background h-screen w-dvw'>
-      <RegisterStudent></RegisterStudent>
+      <RegisterTeacher></RegisterTeacher>
       <h1 className="text-3xl font-bold underline text-background">
         Hello world!
       </h1>
