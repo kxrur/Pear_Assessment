59ca13a89c1d2d218ddf59f2fc085ad5a881bf29
vireshpatel43 <viresh.p2003@gmail.com>
2024-09-22 22:37:20 -0400

    Create repositories for users and roles

--- peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/repository/UserRepository.java
+++ peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/repository/UserRepository.java
@@ -1,4 +1,25 @@
 package com.monaco.peer_assessment_backend.repository;
 
-public class UserRepository {
+import com.monaco.peer_assessment_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * JpaRepository will provide methods for CRUD operations
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+    /**
+     * Custom query to find a User by their username
+     * @param username The username of the desired User
+     * @return the desired User
+     */
+    Optional<User> findByUsername(String username);
+
+    /**
+     * Custom query to find a Student by their studentID
+     * @param studentID The studentID of the desired Student
+     * @return the desired Student
+     */
+    //Optional<User> (long studentID);
 }
