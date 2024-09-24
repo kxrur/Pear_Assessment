711d61b9a5de275949cf7363dc47b961b3dd0fca
vireshpatel43 <viresh.p2003@gmail.com>
2024-09-23 21:21:27 -0400

    Create bean for passwordEncoder

--- peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/configuration/SecurityConfig.java
+++ peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/configuration/SecurityConfig.java
@@ -1,2 +1,14 @@
-package com.monaco.peer_assessment_backend.configuration;public class SecurityConfig {
+package com.monaco.peer_assessment_backend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
