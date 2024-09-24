92e1b838d3e7799f9dad137260f2b2853e8cdc29
KneewYen <n_le2023@live.concordia.ca>
2024-09-24 17:25:52 -0400

    login student

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,22 @@
 import './App.css'
+<<<<<<< HEAD
 import RegisterStudent from './components/views/registration/student'
 import RegisterTeacher from './components/views/registration/teacher'
+=======
+import LoginStudent from './components/views/login-student'
+
+
+>>>>>>> d1911a8 (login student)
 
 function App() {
 
   return (
     <div className='bg-background h-screen w-dvw'>
+<<<<<<< HEAD
       <RegisterTeacher></RegisterTeacher>
+=======
+      <LoginStudent></LoginStudent>
+>>>>>>> d1911a8 (login student)
       <h1 className="text-3xl font-bold underline text-background">
         Hello world!
       </h1>
