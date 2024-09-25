fbcfc8e40357486750a80108ea78acf87a46db2d
vireshpatel43 <viresh.p2003@gmail.com>
2024-09-25 19:10:39 -0400

    Remove security dependency preventing API requests

--- peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/controller/AuthController.java
+++ peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/controller/AuthController.java
@@ -1,21 +1,27 @@
 package com.monaco.peer_assessment_backend.controller;
 
-import com.monaco.peer_assessment_backend.Service.UserService;
+import com.monaco.peer_assessment_backend.service.UserService;
+import com.monaco.peer_assessment_backend.service.impl.UserServiceImpl;
 import com.monaco.peer_assessment_backend.dto.StudentDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
+@AllArgsConstructor
 @RestController
-@RequestMapping("/signIn/")
+@RequestMapping("/api/register")
 public class AuthController {
-    UserService userS = new UserService();
-    @PostMapping("/student-signUp")
-    public ResponseEntity<?> registerStudent(@RequestBody StudentDTO newStudent) {
-        userS.signup(newStudent);
-        return ResponseEntity.ok("Student registered successfully!");
+
+
+    private UserService userService;
+
+
+    @PostMapping
+    public ResponseEntity<StudentDTO> registerStudent(@RequestBody StudentDTO newStudent) {
+        StudentDTO savedStudent = userService.registerStudent(newStudent);
+        return new ResponseEntity<>(savedStudent, HttpStatus.CREATED);
     }
 
 }
--- peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/repository/RoleRepository.java
+++ peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/repository/RoleRepository.java
@@ -2,12 +2,14 @@ package com.monaco.peer_assessment_backend.repository;
 
 import com.monaco.peer_assessment_backend.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 /**
  * JpaRepository will provide methods for CRUD operations on the MySQL database
  */
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     /**
      * Custom query to find a Role by its name
