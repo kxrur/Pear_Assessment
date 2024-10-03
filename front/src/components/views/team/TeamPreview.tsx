2738dadf0379c3a2cd89cd1b3c5b597e2a97cc59
kxrur <kxrury@gmail.com>
2024-10-03 14:55:50 -0400

    change up colors

--- front/src/components/views/team/TeamPreview.tsx
+++ front/src/components/views/team/TeamPreview.tsx
@@ -12,11 +12,11 @@ export const TeamPreview: React.FC<TeamProps> = ({
   teamDescription,
 }) => {
   return (
-    <div className="flex flex-col items-center p-6 bg-accent max-w-xl rounded-lg shadow-lg ">
+    <div className="flex flex-col items-center p-6 bg-secondary max-w-xl rounded-lg shadow-lg ">
       <h1 className="text-4xl font-bold text-background mb-6">{teamName}</h1>
       <div className="flex flex-row space-x-4 h-full max-w-xl ">
         {/* Team Members List */}
-        <div className="w-1/4 bg-foreground p-4 rounded-lg overflow-auto">
+        <div className="w-1/4 bg-accent p-4 rounded-lg overflow-auto">
           {teamMembers.length ? (
             <ul className="space-y-2">
               {teamMembers.slice(0, 5).map((member, index) => (
@@ -25,7 +25,7 @@ export const TeamPreview: React.FC<TeamProps> = ({
                 </li>
               ))}
               {teamMembers.length > 5 && (
-                <li className="text-secondary">...</li>
+                <li className="text-foreground">...</li>
               )}
             </ul>
           ) : (
@@ -33,7 +33,7 @@ export const TeamPreview: React.FC<TeamProps> = ({
           )}
         </div>
         {/* Team Description */}
-        <div className="w-3/4 bg-foreground p-4 rounded-lg ">
+        <div className="w-3/4 bg-accent p-4 rounded-lg ">
           <p className="text-highlight text-ellipsis overflow-hidden line-clamp-6">
             {teamDescription}
           </p>
