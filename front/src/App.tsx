1819dd982f8e70b466538ed6251542f2a2eb84b8
kxrur <kxrury@gmail.com>
2024-10-03 14:39:17 -0400

    added preview component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,44 +5,53 @@ import LoginStudent from './components/views/login/student'
 import RegisterStudent from './components/views/registration/student'
 import RegisterTeacher from './components/views/registration/teacher'
 import Success from './components/views/success'
+import { TeamPreview } from './components/views/team/TeamPreview';
 function App() {
 
   return (
     <Router>
-          <div className='bg-background h-screen w-dvw'>
+      <div className='bg-background h-screen w-dvw'>
 
-            <Routes>
-                {/* Default Route - When user accesses the root URL, show this component */}
-                <Route path="/" element={<LoginStudent />} />
-              <Route path="/teacher"
-                element={
-                  <>
-                  <RegisterTeacher />
-                  <LoginTeacher />
-                  </>
-                }
+        <Routes>
+          <Route path="/teacher"
+            element={
+              <>
+                <RegisterTeacher />
+                <LoginTeacher />
+              </>
+            }
+          />
+
+          <Route path="/student"
+            element={
+              <>
+                <RegisterStudent />
+                <LoginStudent />
+              </>
+            }
+          />
+          <Route path="/success"
+            element={
+              <>
+                <Success />
+              </>
+            }
+          />
+          <Route path="/"
+            element={
+              <>
+                <TeamPreview
+                  teamName="Dev Team"
+                  teamMembers={['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank']}
+                  teamDescription="We are a team of developers working on cutting-edge technology projects. Our focus is on creating efficient, scalable, and innovative solutions. team description Lorem Ipsum is simply dummy text of the printing and  typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of  type and scrambled it to make a type specimen book. "
                 />
+              </>
+            }
+          />
 
-              <Route path="/student"
-                element={
-                  <>
-                  <RegisterStudent />
-                  <LoginStudent />
-                  </>
-                }
-                />
+        </Routes>
 
-              <Route path="/success"
-                element={
-                  <>
-                  <Success />
-                  </>
-                }
-                />
-
-            </Routes>
-
-          </div>
+      </div>
     </Router>
   )
 }
