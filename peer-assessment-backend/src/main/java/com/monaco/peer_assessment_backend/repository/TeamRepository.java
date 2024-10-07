0b03e2916db08b5959270d39382cb4be1e23c84a
vireshpatel43 <viresh.p2003@gmail.com>
2024-10-07 12:19:59 -0400

    Add custom query for StudentRepository and create TeamRepositoy

--- peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/repository/TeamRepository.java
+++ peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/repository/TeamRepository.java
@@ -1,2 +1,7 @@
-package com.monaco.peer_assessment_backend.repository;public class TeamRepository {
+package com.monaco.peer_assessment_backend.repository;
+
+import com.monaco.peer_assessment_backend.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository <Team, Long> {
 }
