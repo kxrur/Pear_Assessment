5c915c9412ff3634470ff416344db76d830b49cf
vireshpatel43 <viresh.p2003@gmail.com>
2024-10-07 12:19:30 -0400

    Add controller for Team creation

--- peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/controller/TeamController.java
+++ peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/controller/TeamController.java
@@ -1,2 +1,25 @@
-package com.monaco.peer_assessment_backend.controller;public class TeamController {
+package com.monaco.peer_assessment_backend.controller;
+
+import com.monaco.peer_assessment_backend.dto.TeamDTO;
+import com.monaco.peer_assessment_backend.service.TeamService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class TeamController {
+    @Autowired
+    private TeamService teamService;
+
+    @PostMapping("/teams/create")
+    public ResponseEntity<?> createTeam(@RequestBody TeamDTO teamDTO) {
+        teamService.createTeam(teamDTO.getProfessorID(), teamDTO.getStudentIDs(), teamDTO.getTeamName());
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
 }
