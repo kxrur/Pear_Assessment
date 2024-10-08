0d66b4b86e4027d348340f6aff5f55e36ed01f44
KneewYen <n_le2023@live.concordia.ca>
2024-10-07 20:21:13 -0400

    corrected

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,17 @@
-import './App.css';
+import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginTeacher from './components/views/login/teacher';
-import LoginStudent from './components/views/login/student';
-import RegisterStudent from './components/views/registration/student';
-import RegisterTeacher from './components/views/registration/teacher';
-import Success from './components/views/success';
+import LoginTeacher from './components/views/login/teacher'
+import LoginStudent from './components/views/login/student'
+import RegisterStudent from './components/views/registration/student'
+import RegisterTeacher from './components/views/registration/teacher'
+import Success from './components/views/success'
 import Begin from './components/views/Begin';
 import { TeamView } from './TeacherPage/TeamView';
+import { TeamPreview } from './components/ui/TeamPreview';
+
+
+
+
 import StudentManagement from './TeacherPage/studentmanagement';
 
 function App() {
@@ -19,7 +24,7 @@ function App() {
     {
       teamName: "Dev Team",
       teamMembers: ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank'],
-      teamDescription: "We are a team of developers working on cutting-edge technology projects.",
+      teamDescription: "We are a team of developers working on cutting-edge technology projects. Our focus is on creating efficient, scalable, and innovative solutions. team description Lorem Ipsum is simply dummy text of the printing and  typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of  type and scrambled it to make a type specimen book. "
     },
     {
       teamName: 'Team Gamma',
@@ -27,37 +32,78 @@ function App() {
       teamDescription: 'Specializing in design and user experience.',
     },
     {
+      teamName: 'Team Alpha',
+      teamMembers: ['Alice', 'Bob', 'Charlie'],
+      teamDescription: 'A team of talented individuals working on innovative projects.',
+    },
+    {
       teamName: 'Team Beta',
       teamMembers: ['David', 'Eve', 'Frank'],
       teamDescription: 'Focused on developing cutting-edge technology solutions.',
     },
+    {
+      teamName: 'Team Gamma',
+      teamMembers: ['Grace', 'Heidi', 'Ivan'],
+      teamDescription: 'Specializing in design and user experience.',
+    },
   ];
-
   return (
+
     <Router>
       <div className='bg-background h-screen w-dvw'>
+
         <Routes>
           {/* Default Route - When user accesses the root URL, show this component */}
           <Route path="/" element={<StudentManagement />} />
           <Route path="/begin" element={<Begin />} />
+          <Route path="/team-preview"
+            element={
+              <>
+                <TeamView teams={teams} />
+              </>
+            }
+          />
           
-          <Route path="/team-preview" element={<TeamView teams={teams} />} />
+          
+         
+          <Route path="/teacher"
+            element={
+              <>
+                <RegisterTeacher />
+                <LoginTeacher />
+              </>
+            }
+          />
+          {/* Sidebar */}
+            
+            
+            <Route path="/teamview" element={<TeamView teams={teams} />} />
+            <Route path="/studentmanagement" element={<StudentManagement />} />
+            
 
-          {/* Teacher and Student Routes */}
-          <Route path="/teacher/login" element={<LoginTeacher />} />
-          <Route path="/teacher/register" element={<RegisterTeacher />} />
-          
-          <Route path="/student/login" element={<LoginStudent />} />
-          <Route path="/student/register" element={<RegisterStudent />} />
-          
-          <Route path="/success" element={<Success />} />
+          <Route path="/student"
+            element={
+              <>
+                <RegisterStudent />
+                <LoginStudent />
+              </>
+            }
+          />
+          <Route path="/success"
+            element={
+              <>
+                <Success />
+              </>
+            }
+          />
+         
 
-          {/* If you want to use TeamView in the teacher's management, adjust as necessary */}
-          <Route path="/studentmanagement" element={<StudentManagement />} />
+
         </Routes>
+
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
