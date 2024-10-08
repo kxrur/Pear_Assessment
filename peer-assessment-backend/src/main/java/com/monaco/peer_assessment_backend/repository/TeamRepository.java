05526f23bd4d8aa505b04e4eabd2dd1bf88bb1e9
Pritthiraj Dey <dpritthiraj@gmail.com>
2024-10-08 16:49:43 -0400

    Update the TeamRepository.java

--- peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/repository/TeamRepository.java
+++ peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/repository/TeamRepository.java
@@ -2,6 +2,11 @@ package com.monaco.peer_assessment_backend.repository;
 
 import com.monaco.peer_assessment_backend.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface TeamRepository extends JpaRepository <Team, Long> {
+    List<Team> findByStudents_Id(Long studentId);
 }
