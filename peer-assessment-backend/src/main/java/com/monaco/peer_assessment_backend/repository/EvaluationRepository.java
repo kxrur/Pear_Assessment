357d13eb00dbf554d14b6681532ec81353627b25
Pritthiraj Dey <dpritthiraj@gmail.com>
2024-10-10 14:37:14 -0400

    Created a Repository called EvaluationRepository.java

--- peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/repository/EvaluationRepository.java
+++ peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/repository/EvaluationRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface EvaluationRepository extends JpaRepository<Evaluation, Long>{
     List<Evaluation> findByEvaluator(Student evaluator);
+    
 }
