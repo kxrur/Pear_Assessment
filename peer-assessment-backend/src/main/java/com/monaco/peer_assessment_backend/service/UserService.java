3545bf03174f201b102c63b296772d0435e59a00
kxrur <kxrury@gmail.com>
2024-10-20 13:14:15 -0400

    maybe fix backend compilation

--- peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/service/UserService.java
+++ peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/service/UserService.java
@@ -4,17 +4,18 @@ import com.monaco.peer_assessment_backend.dto.ProfessorDTO;
 import com.monaco.peer_assessment_backend.dto.StudentDTO;
 import com.monaco.peer_assessment_backend.dto.UserDTO;
 import com.monaco.peer_assessment_backend.exception.DuplicateUserException;
+import com.monaco.peer_assessment_backend.exception.UserNotFoundException;
 
 public interface UserService {
-//    public User createUser(User  newUser);
-//    public List<User> getAllUsers();
-//    public User updateUser(int id, User newUser);
-//    public User deleteUser(int id);
+  // public User createUser(User newUser);
+  // public List<User> getAllUsers();
+  // public User updateUser(int id, User newUser);
+  // public User deleteUser(int id);
 
-    StudentDTO registerStudent(StudentDTO studentDTO) throws DuplicateUserException;
+  StudentDTO registerStudent(StudentDTO studentDTO) throws DuplicateUserException;
 
-    ProfessorDTO registerProfessor(ProfessorDTO professorDTO) throws DuplicateUserException;
+  ProfessorDTO registerProfessor(ProfessorDTO professorDTO) throws DuplicateUserException;
 
-    UserDTO getUserById(Long id) throws UserNotFoundException;
+  UserDTO getUserById(Long id) throws UserNotFoundException;
 
 }
