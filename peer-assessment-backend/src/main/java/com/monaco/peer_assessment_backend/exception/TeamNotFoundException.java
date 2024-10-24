dfe4af7c8ce666c4c6b554b2f7b77de14923c207
vireshpatel43 <viresh.p2003@gmail.com>
2024-10-24 16:48:12 -0400

    Remove old files

--- peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/exception/TeamNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.monaco.peer_assessment_backend.exception;
-
-public class TeamNotFoundException extends RuntimeException {
-    public TeamNotFoundException(String message) {
-        super(message);
-    }
-}
