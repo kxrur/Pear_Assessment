7e82e138b740cce608524e37522b30cb3d5c894a
Mach2-1 <ch.mathieu2004@hotmail.com>
2024-10-27 16:21:20 -0400

    fixed the file path not woring if you are in a specific directory

--- peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/service/impl/StorageProperties.java
+++ peer-assessment-backend/src/main/java/com/monaco/peer_assessment_backend/service/impl/StorageProperties.java
@@ -3,13 +3,17 @@ package com.monaco.peer_assessment_backend.service.impl;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @ConfigurationProperties("storage")
 public class StorageProperties {
 
     /**
      * Folder location for storing files
      */
-    private String location = "src/main/resources/studentFiles";
+    // private String location = "src/main/resources/studentFiles";
+    private String location = String.valueOf(getResourcePath("studentFiles"));
 
     public String getLocation() {
         return location;
@@ -17,6 +21,15 @@ public class StorageProperties {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+    public static Path getResourcePath(String folderName) {
+        Path path = Paths.get("src/main/resources", folderName);
+
+        // If path doesn't exist, try the alternative path
+        if (!path.toFile().exists()) {
+            path = Paths.get("peer-assessment-backend/src/main/resources", folderName);
+        }
+        return path;
     }
 
 }
