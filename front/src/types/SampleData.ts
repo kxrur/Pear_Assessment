013d478aa8626e6562a2fdd446b5122c18afba9b
KneewYen <n_le2023@live.concordia.ca>
2024-10-27 01:05:12 -0400

    Select team drop down component for teacher modified

--- front/src/types/SampleData.ts
+++ front/src/types/SampleData.ts
@@ -10,31 +10,19 @@ export const teams: Team[] = [
   {
     teamName: "Dev Team",
     teamMembers: ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank'],
-    teamDescription: "We are a team of developers working on cutting-edge technology projects. Our focus is on creating efficient, scalable, and innovative solutions. team description Lorem Ipsum is simply dummy text of the printing and  typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of  type and scrambled it to make a type specimen book. ",
+    teamDescription: "We are a team of developers working on cutting-edge technology projects. Our focus is on creating efficient, scalable, and innovative solutions. team description Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     professorId: "",
   },
   {
     teamName: 'Team Gamma',
     teamMembers: ['Grace', 'Heidi', 'Ivan'],
     teamDescription: 'Specializing in design and user experience.',
-    professorId: "",
-  },
-  {
-    teamName: 'Team Alpha',
-    teamMembers: ['Alice', 'Bob', 'Charlie'],
-    teamDescription: 'A team of talented individuals working on innovative projects.',
     professorId: "",
   },
   {
     teamName: 'Team Beta',
     teamMembers: ['David', 'Eve', 'Frank'],
     teamDescription: 'Focused on developing cutting-edge technology solutions.',
-    professorId: "",
-  },
-  {
-    teamName: 'Team Gamma',
-    teamMembers: ['Grace', 'Heidi', 'Ivan'],
-    teamDescription: 'Specializing in design and user experience.',
     professorId: "",
   },
 ];
