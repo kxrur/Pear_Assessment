82ff4a18675a5a1001e03cd595ad94958f378b2a
KneewYen <n_le2023@live.concordia.ca>
2024-11-07 16:50:46 -0500

    took off the comments

--- app/front/src/views/Summary.tsx
+++ app/front/src/views/Summary.tsx
@@ -65,67 +65,19 @@ export default function Summary() {
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{student.averageGrade}</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                   {student.cooperation.stars}
-                  {student.cooperation.comment && (
-                    <svg
-                      onClick={() => openModal(student.cooperation.comment)}
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="ml-2 h-5 w-5 text-blue-500 cursor-pointer inline"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                    </svg>
-                  )}
+
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                   {student.conceptual.stars}
-                  {student.conceptual.comment && (
-                    <svg
-                      onClick={() => openModal(student.conceptual.comment)}
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="ml-2 h-5 w-5 text-blue-500 cursor-pointer inline"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                    </svg>
-                  )}
+
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                   {student.practical.stars}
-                  {student.practical.comment && (
-                    <svg
-                      onClick={() => openModal(student.practical.comment)}
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="ml-2 h-5 w-5 text-blue-500 cursor-pointer inline"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                    </svg>
-                  )}
+
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                   {student.workEthic.stars}
-                  {student.workEthic.comment && (
-                    <svg
-                      onClick={() => openModal(student.workEthic.comment)}
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="ml-2 h-5 w-5 text-blue-500 cursor-pointer inline"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                    </svg>
-                  )}
+
                 </td>
               </tr>
             ))}
