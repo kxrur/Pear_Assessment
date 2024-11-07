0fe125b669c989913df8f82274e5b03233282412
KneewYen <n_le2023@live.concordia.ca>
2024-11-07 16:14:49 -0500

    fixed routing

--- app/front/src/views/Summary.tsx
+++ app/front/src/views/Summary.tsx
@@ -29,21 +29,33 @@ export default function Summary() {
 
         <table className="min-w-full bg-gray-100 border border-gray-100">
           <thead>
-            <tr>
-              <th className="px-4 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">No</th>
-              <th className="px-4 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">First Name</th>
-              <th className="px-6 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">Last Name</th>
-              <th className="px-6 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">ID</th>
-              <th className="px-6 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">Team Name</th>
-              <th className="px-2 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">Average Peer Grade</th>
-              <th className="px-3 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">Cooperation</th>
-              <th className="px-3 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">Conceptual</th>
-              <th className="px-3 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">Practical</th>
-              <th className="px-3 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">Work Ethic</th>
-            </tr>
+          <tr>
+            <th className="px-4 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">No</th>
+            <th className="px-4 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">First
+              Name
+            </th>
+            <th className="px-6 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">Last
+              Name
+            </th>
+            <th className="px-6 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">ID</th>
+            <th className="px-6 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">Team
+              Name
+            </th>
+            <th className="px-2 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">Average
+              Peer Grade
+            </th>
+            <th className="px-3 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">Cooperation</th>
+            <th className="px-3 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">Conceptual</th>
+            <th className="px-3 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">Practical</th>
+            <th className="px-3 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">Work
+              Ethic
+            </th>
+            <th className="px-3 py-2 border-b border-gray-300 bg-gray-200 text-left text-sm font-semibold text-gray-600">Peers answered
+            </th>
+          </tr>
           </thead>
           <tbody>
-            {summaryData.map((student, index) => (
+          {summaryData.map((student, index) => (
               <tr key={student.studentId}>
                 <td className="px-4 py-2 whitespace-nowrap text-sm text-gray-500">{index + 1}</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{student.firstName}</td>
